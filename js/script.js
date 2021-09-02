6ed4d31fad993026a3056639eed20b08e01c0b6a
Jean <jeanramalho.dev@gmail.com>
2021-09-02 16:26:41 -0300

    codificador pronto

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 const inputOriginal = document.getElementById('input-original')
 const inputOriginalBase = document.getElementById('input-original-Base64')
-const crifrador = document.getElementById('cifrador')
+const cifrador = document.getElementById('cifrador')
 const cifradorBase = document.getElementById('cifradorBase64')
 const resultado = document.getElementById('resultado')
 var resultadoBase = document.getElementById('resultadoBase64')
-var range = document.getElementById('range')
+const range = document.getElementById('range')
 const cod = document.getElementById('codificar')
 const decod = document.getElementById('decodificar')
 var cifraCesar = document.getElementById('cifraCesar')
@@ -16,6 +16,8 @@ var cesar = document.getElementById('cesar')
 var base = document.getElementById('base')
 var deslocamento = document.getElementById('desloca')
 var texto = inputOriginalBase.value
+var btn = document.getElementById('codOuDecod')
+var btnBase = document.getElementById('codOuDecodBase')
 
 //determinar escolha do criptador
 cifraCesar.addEventListener('click', function () {
@@ -31,57 +33,75 @@ base64.addEventListener('click', function () {
 cod.addEventListener('change', (e) => {
     if (cod.checked == true) {
         decod.checked = false
+        btn.textContent = 'CODIFICAR'
     }
 })
 decod.addEventListener('change', (e) => {
     if (decod.checked == true){
         cod.checked = false
+        btn.textContent = 'DECODIFICAR'
     }
 })
 
 
-// transforma o range em numero
-var novoRange = parseInt(range.value)
 
-//transforma tudo em maiusculo
-const tudoMaiusculo = () => {
-    const arrayDePalavras = [...inputOriginal.value.toUpperCase()]
-    printChar(0, arrayDePalavras)    
+
+//mostra o resultado com base na escolha codificar e decodificar
+cifrador.addEventListener('submit', (e) => {
+    if(cod.checked == true) {
+        toCifra(inputOriginal.value, +range.value)
+       
+        
+    } else if (decod.checked == true) {
+        toNormal(inputOriginal.value, +range.value)
+    }
+})
+
+//função para codificar o texto
+function toCifra(lC, num){
+    var coded = ""
+    let asc
+    let msg = lC.toLowerCase()
+    for (i=0; i<msg.length; i++){
+        if (msg[i] == " ")
+        coded += " "
+        else {
+            if (msg.charCodeAt(i)+num>122)
+            asc = msg.charCodeAt(i)+num-26
+            else
+            asc = msg.charCodeAt(i)+num
+            coded += String.fromCharCode(asc)
+        }
+    }
+return resultado.value = coded
 }
 
-//escreve no campo o resultado de acordo com a escolha de codificar e decodificar
-const printChar = (currentLetterIndex, arrayDePalavras) => {
-    if(arrayDePalavras.length === currentLetterIndex) {
-    return 
+//função para decodificar o texto
+function toNormal(lC, num){
+    var decoded = ""
+    let letra
+    let msg = lC.toLowerCase()
+    for (i=0; i<msg.length; i++){
+        if (msg[i] == " ")
+        decoded += " "
+        else {
+            if (msg.charCodeAt(i)-num<97)
+            letra = msg.charCodeAt(i)-num+26
+            else
+            letra = msg.charCodeAt(i)-num
+            decoded += String.fromCharCode(letra)
+        }
+    }
+return resultado.value = decoded
 }
-if(cod.checked == true) {
-    inputOriginal.value = inputOriginal.value.substring(1)
-    const spanChar = document.createElement("span")
-    resultado.appendChild(spanChar)
-    const charSemCodificar = arrayDePalavras[currentLetterIndex]
-    spanChar.innerHTML = alfabeto.includes(charSemCodificar) ?
-    alfabeto[(alfabeto.indexOf(charSemCodificar)+ parseInt(range.value)) % alfabeto.length] : 
-    charSemCodificar    
-    printChar(currentLetterIndex + 1, arrayDePalavras)
-} else if (decod.checked == true) {
-    inputOriginal.value = inputOriginal.value.substring(1)
-    const spanChar = document.createElement("span")
-    resultado.appendChild(spanChar)
-    const charSemCodificar = arrayDePalavras[currentLetterIndex]
-    spanChar.innerHTML = alfabeto.includes(charSemCodificar) ?
-    alfabeto[(alfabeto.indexOf(charSemCodificar)- parseInt(range.value)) % alfabeto.length] : 
-    charSemCodificar    
-    printChar(currentLetterIndex + 1, arrayDePalavras)
-}
-}
+
 
 //previne comportamento padrão do submit
 const submit = e => {
     e.preventDefault()
     resultado.innerHTML = ''
-    tudoMaiusculo()
 }
-crifrador.onsubmit = submit
+cifrador.onsubmit = submit
 
 
 
@@ -92,11 +112,13 @@ crifrador.onsubmit = submit
 codBase.addEventListener('change', (e) => {
     if (codBase.checked == true) {
         decodBase.checked = false
+        btnBase.textContent = 'CODIFICAR'
     }
 })
 decodBase.addEventListener('change', (e) => {
     if (decodBase.checked == true){
         codBase.checked = false
+        btnBase.textContent = 'DECODIFICAR'
     }
 })
 
